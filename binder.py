527c40410f6e0324088ac0e5b7ac5538afff10d1
Maykel Moya <mmoya@mmoya.org>
2012-05-01 22:52:41 +0200

    Implement UDP ping/pong in binder

--- binder.py
+++ binder.py
@@ -7,6 +7,7 @@ import socket
 import sys
 
 srvsockets = {}
+udpsockets = {}
 conns = {}
 
 def load_peers(filename, _id):
@@ -18,7 +19,7 @@ def load_peers(filename, _id):
             fields = line.strip().split()
             if fields[0] == _id:
                 continue
-            peers.append((fields[1], int(fields[3])))
+            peers.append((fields[1], int(fields[2]), int(fields[3])))
     return tuple(peers)
 
 def main():
@@ -26,14 +27,24 @@ def main():
 
     epoll = select.epoll()
     for peer in peers:
+        tcp_peer = (peer[0], peer[2])
         sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print("Binding to TCP {0}:{1}".format(*peer))
-        sk.bind(peer)
+        print("Binding to TCP {0}:{1}".format(*tcp_peer))
+        sk.bind(tcp_peer)
         sk.listen(1)
         sk.setblocking(0)
         epoll.register(sk.fileno(), select.EPOLLIN)
         srvsockets[sk.fileno()] = sk
+
+        udp_peer = (peer[0], peer[1])
+        sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print("Binding to UDP {0}:{1}".format(*udp_peer))
+        sk.bind(udp_peer)
+        sk.setblocking(0)
+        epoll.register(sk.fileno(), select.EPOLLIN)
+        udpsockets[sk.fileno()] = sk
 
     while True:
         events = epoll.poll(-1)
@@ -45,6 +56,14 @@ def main():
                 conn.setblocking(0)
                 epoll.register(conn.fileno(), select.EPOLLIN)
                 conns[conn.fileno()] = (conn, addr)
+            elif fileno in udpsockets.keys():
+                sk = udpsockets[fileno]
+                _input, addr = sk.recvfrom(1024)
+                print("From UDP {1}:{2}: {0}".format(_input, *addr))
+                if _input.strip() == b'ping':
+                    pong = b'pong\n'
+                    print("Sending {0} to UDP {1}:{2}".format(pong, *addr))
+                    sk.sendto(pong, addr)
             elif event & select.EPOLLIN:
                 conn, addr = conns[fileno]
                 _input = conn.recv(1024)
